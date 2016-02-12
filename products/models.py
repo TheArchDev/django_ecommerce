cffa1d0565adbb036d6aaa8547657ca82a88561b
Alex Archer <archeralex@gmail.com>
2016-02-12 09:40:57 -0600

    Adding shopping cart functionality

--- products/models.py
+++ products/models.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
 class Product(models.Model):
       name = models.CharField(max_length=200)
@@ -8,3 +9,10 @@ class Product(models.Model):
       price = models.FloatField()
       def __str__(self):
       	  return self.name
+
+class Order(models.Model):
+      status = models.IntegerField()
+      user = models.ForeignKey(User)
+      items = models.ManyToManyField(Product)
+      def __str__(self):
+            return str(self.status)
--- products/views.py
+++ products/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render,redirect
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.db.models import Q
 
+#think could have either .models or products.models
+from .models import Order
+from django.http import HttpResponse
 
 
 def index(request):
@@ -35,7 +38,7 @@ def index(request):
 	#alternatively could have done two separate if statements. One to check for pricemore, then the altered all_products out of that can feed into the next if statement which will check for priceless, before amending all_products as appropriate
 
 	#it's saying 10 max
-	paginator = Paginator(all_products,10)
+	paginator = Paginator(all_products,5)
 
 	#Line below is basically saying get this value out of the dictionary.
 	#request.GET is like/derived from a dictionary. This is to do with the HTTP GET, operating on the string.
@@ -57,3 +60,79 @@ def index(request):
 def product_page(request,id):
 	p = Product.objects.get(pk=id)
 	return render(request,'products/product.html', {'product': p})
+
+def add_to_cart(request, product_id):
+	print "auth",request.user.is_authenticated()
+	if request.user.is_authenticated():
+		#request is an object. request.user is an object
+		# 1. only returns orders that are the current user's 
+		# 2. only returns orders that are active (status=1)
+		orders=Order.objects.filter(user=request.user,status=1)
+		# SELECT * FROM product_order WHERE user_id = 1 AND status = 1;
+		#orders is an array that sometimes contains an object
+		# if there are no active orders for the current user
+		if len(orders) == 0:
+			# creates the order that is active and associated with the current user
+			order=Order(status=1, user=request.user)
+			order.save()
+			# INSERT INTO product_order (user_id, status) VALUES (eg1,1);
+			# grab product instance from products that has the right id
+			product = Product.objects.get(id=product_id)
+			# SELECT * FROM product_product WHERE id = 1;
+
+			# adds product instance to current user's active order instance
+			order.items.add(product)
+			# INSERT INTO product_order_item (product_id,order_id) VALUES (1,1);
+
+			#return HttpResponse("An open order did not exist. We created a new order and added the product to it.")
+			print "Signal 1"
+			return redirect("/products/cart")
+
+		# if the current user already has an active order
+		else:
+			# get user's active order instance out of array
+			order = orders[0]
+			#get the product
+			product = Product.objects.get(id=product_id)
+			#add 2 user's active order instance
+			order.items.add(product)
+			
+			#return HttpResponse("An open order already existed, we added a product to that open order.")
+			print "Signal 2"
+			return redirect("/products/cart")
+
+	return redirect("/login")
+
+def remove_from_cart(request, product_id):
+	print "product_id", product_id
+
+	orders = Order.objects.filter(user=request.user,status=1)
+	order = orders[0]
+	order.save()
+	order.items.remove(product_id)
+
+	#return HttpResponse("Clicked to remove item from cart")
+	return redirect("/products/cart")
+
+def cart(request):
+	if not request.user.is_authenticated():
+		print "Trigger cart login"
+		return redirect("/login")
+	
+	#Need to add in an if statement such that if browser comes to this site without having placed an order
+
+	orders = Order.objects.filter(user=request.user,status=1)
+
+	order = orders[0]
+	order.save()
+
+	order_items = order.items.all()
+
+	cart_total=sum([order_item.price for order_item in order_items])
+
+	return render(request,"products/cart.html", {"order_items": order_items, "cart_total":cart_total})
+	#return HttpResponse("You're successfully logged into your cart")
+
+
+
+
